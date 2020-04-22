67a9417621d0b83690e71364dd306ab70cd7fa3a
Minh Nguyen Binh <minhnb9897@gmail.com>
2020-04-23 00:24:37 +0700

    #1

--- src/app/admin/admin-dashboard/admin-product/product/product.component.ts
+++ src/app/admin/admin-dashboard/admin-product/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'app/services/Products/product.service';
 
 @Component({
   selector: 'app-product',
@@ -7,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private productService : ProductService
+  ) { }
 
   ngOnInit(): void {
   
   }
+
+
+  
 
 }
